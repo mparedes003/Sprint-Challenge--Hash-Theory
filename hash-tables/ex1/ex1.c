fd3305f0b5d8e4baad18cd36faf2ab3feecd9254
mparedes003 <onnabugeishadev@gmail.com>
2019-04-05 10:15:33 -0700

    ex1 done. It prints out the correct output.

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "hashtable.h"
+#include "hashtable.c" // import hashtable.c so you have access to the hash_table functions
 #include "ex1.h"
 
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
@@ -8,15 +9,39 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
   HashTable *ht = create_hash_table(16);
 
   // YOUR CODE HERE
+  // Allocate memory for the answer that is to be returned
+  Answer *answer = malloc(sizeof(Answer));
+
+  // Use a for loop to construct the hash table
+  for (int i = 0; i < length; i++)
+  {
+    // Assign the value of a pair in ht to get_index
+    int return_index = hash_table_retrieve(ht, weights[i]);
+
+    // If the key exists in the hash table
+    if (return_index != -1)
+    {
+      answer->index_1 = i;
+      answer->index_2 = return_index;
+      return answer;
+    }
+
+    int key = (limit - weights[i]);
+    // Insert a pair in ht where value = i
+    hash_table_insert(ht, key, i);
+  }
 
   return NULL;
 }
 
 void print_answer(Answer *answer)
 {
-  if (answer != NULL) {
+  if (answer != NULL)
+  {
     printf("%d %d\n", answer->index_1, answer->index_2);
-  } else {
+  }
+  else
+  {
     printf("NULL\n");
   }
 }
@@ -28,22 +53,22 @@ int main(void)
   // TEST 1
   int weights_1 = {9};
   Answer *answer_1 = get_indices_of_item_weights(&weights_1, 1, 9);
-  print_answer(answer_1);  // NULL
+  print_answer(answer_1); // NULL
 
   // TEST 2
   int weights_2[] = {4, 4};
-  Answer* answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
-  print_answer(answer_2);  // {1, 0}
+  Answer *answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
+  print_answer(answer_2); // {1, 0}
 
   // TEST 3
   int weights_3[] = {4, 6, 10, 15, 16};
-  Answer* answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
-  print_answer(answer_3);  // {3, 1}
+  Answer *answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
+  print_answer(answer_3); // {3, 1}
 
   // TEST 4
   int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
-  Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
-  print_answer(answer_4);  // {6, 2}
+  Answer *answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
+  print_answer(answer_4); // {6, 2}
 
   return 0;
 }
